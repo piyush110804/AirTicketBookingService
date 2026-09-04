75b6f4441d966e81203d273924d61e270adfc9fc
piyush singh <piyush110804@gmail.com>
2026-09-05 02:24:19 +0530

    added basic setup

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const app=express();
+const {PORT}=require('./config/serverConfig');
+const apiRoutes=require('./routers/index');
+const db=require('./models/index');
+const bodyParser=require('body-parser');
+
+const SetupAndStartServer=()=>{
+  app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use('/api',apiRoutes);
+ app.listen(PORT,()=>{
+  if(process.env.DB_SYNC){
+    db.sequelize.sync({alter:true});
+  }
+  console.log(`Server Started at PORT ${PORT}`);
+ });
+
+}
+SetupAndStartServer();
