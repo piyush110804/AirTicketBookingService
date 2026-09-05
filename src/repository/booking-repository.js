cbf3dd3a8aa078fdb2dc77cc870bd0b082de3f1e
piyush singh <piyush110804@gmail.com>
2026-09-05 21:37:26 +0530

    added create booking

---
+++ src/repository/booking-repository.js
@@ -0,0 +1,21 @@
+const {AppError,ValidationError}=require('../utils/errors/index');
+const {Booking}=require('../models/index');
+const {StatusCodes}=require('http-status-codes');
+
+class BookingRepository{
+  async create(data){
+    try {
+      const booking =await Booking.create(data);
+      return booking; 
+    } catch (error) {
+      if(error.name==='SequelizeValidationError'){
+        throw new ValidationError(error);
+      }
+      throw new AppError('Repository Error',
+        'Cannot create booking',
+        'There was some issue creating the booking',
+        StatusCodes.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
+}
